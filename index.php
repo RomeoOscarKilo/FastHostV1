7592c6d318006c0d85837fb87f49db4d8af3db36
alex seddon <alexander.seddon@ymail.com>
2018-11-11 18:21:49 +0000

    Global Name updates,file name updates, ref updates

--- index.php
+++ index.php
@@ -2,68 +2,64 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8" />
-	<title> Fasthost's New TLD </title>
-	<link rel="stylesheet" href="main.css">
+  <meta charset="utf-8" />
+  <title> Fasthost's New TLD </title>
+  <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-	<div id="completewrap">
-		<header id="topheader">
-			 <h1 id="mainheader">Welcome to Fasthost
-				 <a href="index.php">
-				 <img id="logo" src="Images/Fathost_logo.png" title="Click to return home" alt="Fasthost logo">
-				 </a>
-			 </h1><!-- -->
-
-		</header>
-		<Nav id="topmenu">
-			<ul>
-				<li><a href="Welcome_Page.php" title="Takes you to the welcome page" >Home</a></li>
-				<li><a href="php\Reg_Page.php" title="Takes you to the registration page" >Register</a></li>
-				<li><a href="php\Login_Page.php" title="Takes you to the login page" >Login </a></li>
-			</ul>
-		</nav>
-		<div id="new_div">
-			<section id="mainsection">
-				<article >
-					<header>
-						<hgroup>
-							<h1 id="Firstpost">New Top level Domains!</h1>
-							<h2>They are new, secure and highspeed</h2>
-						</hgroup>
-					</header>
-					<p> Fan of .com .org and .uk? well were are fan of .you! So register to show interest for our new TLD's</p>
-					<footer>
-						<p>Coming soon, to fasthosts product line</p>
-					</footer>
-				</article>
-
-				<article >
-					<header>
-						<hgroup>
-							<h1>New login and registration page's!</h1>
-							<h2>Secure and safe login for all users</h2>
-						</hgroup>
-					</header>
-					<p>We have deployed and optimised a new login and registration page for users looking to control and explore our new product</p>
-					<footer>
-						<p>Coming soon, to fasthosts product line</p>
-					</footer>
-				</article>
-			</section>
-
-
-			<aside id="sidenews">
-				<div id="news"><h4>New TLD's </h4> New range of top level domains out for purchase</div>
-
-			</aside>
-
-		</div>
-		<footer id="footer">
-			All rights belong to Fasthosts ltd. Contact fake@email.com for any details
-		</footer>
-	</div>
+  <div id="completewrap">
+    <header id="topheader">
+      <h1 id="mainheader">Welcome to Fasthost
+        <a href="index.php">
+          <img id="logo" src="Images/Fathost_logo.png" title="Click to return home" alt="Fasthost logo">
+        </a>
+      </h1>
+    </header>
+    <Nav id="topmenu">
+      <ul>
+        <li><a href="index.php" title="Takes you to the welcome page">Home</a></li>
+        <li><a href="php\RegPage.php" title="Takes you to the registration page">Register</a></li>
+        <li><a href="php\LogPage.php" title="Takes you to the login page">Login </a></li>
+      </ul>
+    </nav>
+    <div id="MainInfo">
+      <section id="mainsection">
+        <article>
+          <header>
+            <hgroup>
+              <h1 id="Firstpost">New Top level Domains!</h1>
+              <h2>They are new, secure and highspeed</h2>
+            </hgroup>
+          </header>
+          <p> Fan of .com .org and .uk? well were are fan of .you! So register to show interest for our new TLD's</p>
+          <footer>
+            <p>Coming soon, to fasthosts product line</p>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <hgroup>
+              <h1>New login and registration page's!</h1>
+              <h2>Secure and safe login for all users</h2>
+            </hgroup>
+          </header>
+          <p>We have deployed and optimised a new login and registration page for users looking to control and explore our new product</p>
+          <footer>
+            <p>Coming soon, to fasthosts product line</p>
+          </footer>
+        </article>
+      </section>
+      <aside id="sidenews">
+        <div id="news">
+          <h4>New TLD's </h4> New range of top level domains out for purchase
+        </div>
+      </aside>
+    </div>
+    <footer id="footer">
+      All rights belong to Fasthosts ltd. Contact fake@email.com for any details
+    </footer>
+  </div>
 </body>
 
 </html>
--- php/LogPage.php
+++ php/LogPage.php
@@ -4,16 +4,12 @@
 <head>
 	<meta charset="utf-8" />
 	<title> Fasthost's Login Page </title>
-	<link rel="stylesheet" href="..\css\main_login.css">
+	<link rel="stylesheet" href="..\css\LogPage.css">
 	<link href="https://use.fontawesome.com/releases/v5.0.2/css/all.css" rel="stylesheet">
 	<!-- https://fontawesome.com/get-started/web-fonts-with-css
       https://fontawesome.com/how-to-use/web-fonts-with-css
   -->
-
 </head>
-
-
-
 <body>
 	<div id="completewrap">
 		<header id="topheader">
@@ -22,48 +18,30 @@
 				 <img id="logo" src="../Images/Fathost_logo.png" title="Click to return home" alt="Fasthost logo">
 				 </a>
 			 </h1>
-			<!-- -->
-
 		</header>
 		<Nav id="topmenu">
 			<ul>
 				<li><a href="../index.php" title="Takes you to the welcome page">Home</a></li>
-				<li><a href="Reg_Page.php" title="Takes you to the registration page">Register</a></li>
-				<li><a href="Login_Page.php" title="Takes you to the login page">Login </a></li>
+				<li><a href="RegPage.php" title="Takes you to the registration page">Register</a></li>
+				<li><a href="LogPage.php" title="Takes you to the login page">Login </a></li>
 			</ul>
 		</nav>
-		<div id="new_div">
+		<div id="CentreBox">
 			<section id="mainsection">
-
-
-
-
 				<div id="Loginbox">
-
 					<h4 id="boxtitle">Login to your dashboard</h4>
-
-
 					<form id="Login">
 						Username:<br>
 						<i class="fas fa-user"></i> <input type="text" placeholder="Enter your Username"> Password:
-
 						<br>
 						<i class="fas fa-key"></i> <input type="password" placeholder="Enter your Password">
 						<input type="submit" value="Submit">
-
-
 					</form>
 				</div>
-
-
-
-
-
 		</div>
 		<footer id="footer">
 			All rights belong to Fasthosts ltd. Contact fake@email.com for any details
 		</footer>
 	</div>
 </body>
-
 </html>
