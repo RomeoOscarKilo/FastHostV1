e340e0a8c73c9e3eec63a5f99b91da2854f97cb3
alex seddon <alexander.seddon@ymail.com>
2018-11-11 16:58:54 +0000

    This was the needed conversion to php

--- php/Login_Page.php
+++ php/Login_Page.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<title> Fasthost's Login Page </title>
-	<link rel="stylesheet" href="..\css\main_reg.css">
+	<link rel="stylesheet" href="..\css\main_login.css">
 	<link href="https://use.fontawesome.com/releases/v5.0.2/css/all.css" rel="stylesheet">
 	<!-- https://fontawesome.com/get-started/web-fonts-with-css
       https://fontawesome.com/how-to-use/web-fonts-with-css
@@ -18,7 +18,7 @@
 	<div id="completewrap">
 		<header id="topheader">
 			<h1 id="mainheader">Welcome to Fasthost
-				 <a href="Welcome_Page.html">
+				 <a href="Welcome_Page.php">
 				 <img id="logo" src="../Images/Fathost_logo.png" title="Click to return home" alt="Fasthost logo">
 				 </a>
 			 </h1>
@@ -27,32 +27,28 @@
 		</header>
 		<Nav id="topmenu">
 			<ul>
-				<li><a href="../index.html" title="Takes you to the welcome page">Home</a></li>
-				<li><a href="Reg_Page.html" title="Takes you to the registration page">Register</a></li>
-				<li><a href="Login_Page.html" title="Takes you to the login page">Login </a></li>
+				<li><a href="../index.php" title="Takes you to the welcome page">Home</a></li>
+				<li><a href="Reg_Page.php" title="Takes you to the registration page">Register</a></li>
+				<li><a href="Login_Page.php" title="Takes you to the login page">Login </a></li>
 			</ul>
 		</nav>
 		<div id="new_div">
 			<section id="mainsection">
 
 
+
+
 				<div id="Loginbox">
 
-					<h4 id="boxtitle">Register an account</h4>
+					<h4 id="boxtitle">Login to your dashboard</h4>
 
 
 					<form id="Login">
-
-						Email:<br>
-						<i class="fas fa-envelope"></i> <input type="email" placeholder="Enter your Email"> Username:
-
-
-						<br>
+						Username:<br>
 						<i class="fas fa-user"></i> <input type="text" placeholder="Enter your Username"> Password:
-						<br>
-						<i class="fas fa-key"></i> <input type="password" placeholder="Enter your Password"> Confirm Your Password<br>
-						<i class="fas fa-key"></i> <input type="password" placeholder="Confirm your Password">
 
+						<br>
+						<i class="fas fa-key"></i> <input type="password" placeholder="Enter your Password">
 						<input type="submit" value="Submit">
 
 
