ed8f4e344ee6a579ce5957da2a47666cf086f0a5
alex seddon <alexander.seddon@ymail.com>
2019-01-27 22:57:30 +0000

    .

--- JavaScript/AgeValidate.js
+++ JavaScript/AgeValidate.js
@@ -4,26 +4,21 @@ function validateForm(){
   if(isNaN(age) || age <16 || age > 100){
     response = "Age is invalid";
     document.getElementById("error").innerHTML=response;
-    return false;
-  }
+    return false;}
   var password , passwordc;
   password = document.forms["Registration"]["password"].value;
   passwordc = document.forms["Registration"]["passwordc"].value;
-  if( password == passwordc){
-  }else {
+  if( password == passwordc){}else {
     response = "Passwords did not match";
     document.getElementById("error").innerHTML=response;
-    return false;
-  }                                                           // check after to make sure user pass is corrected
+    return false;}                                                           // check after to make sure user pass is corrected
   password = document.forms["Registration"]["password"].value; //include some client side forced secuirty to prevent user error
   if (password.length < 5){
     response = "Password is too short";
     document.getElementById("error").innerHTML=response;
-    return false;
+    return false;}
     username = document.forms["Registration"]["username"].value; //no 2 letter usernames
     if (username.length < 3 || username.length > 20 ){
       response = "Username is too short";
       document.getElementById("error").innerHTML=response;
-      return false;
-  }
-}
+      return false;}}
