eb599352f706b18b7ee886f7f7ff9fc7d48f841e
alex seddon <alexander.seddon@ymail.com>
2019-01-27 22:49:38 +0000

    yep

--- JavaScript/AgeValidate.js
+++ JavaScript/AgeValidate.js
@@ -10,27 +10,20 @@ function validateForm(){
   password = document.forms["Registration"]["password"].value;
   passwordc = document.forms["Registration"]["passwordc"].value;
   if( password == passwordc){
-
   }else {
     response = "Passwords did not match";
     document.getElementById("error").innerHTML=response;
     return false;
-}
-
-                                                               // check after to make sure user pass is corrected
+  }                                                           // check after to make sure user pass is corrected
   password = document.forms["Registration"]["password"].value; //include some client side forced secuirty to prevent user error
   if (password.length < 5){
     response = "Password is too short";
     document.getElementById("error").innerHTML=response;
     return false;
-
     username = document.forms["Registration"]["username"].value; //no 2 letter usernames
     if (username.length < 3 || username.length > 20 ){
       response = "Username is too short";
       document.getElementById("error").innerHTML=response;
       return false;
-
-
   }
-
 }
