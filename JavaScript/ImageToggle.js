9a5e23c1660ab71e1746224edc17243a1075ce7d
alex seddon <alexander.seddon@ymail.com>
2019-01-31 23:12:51 +0000

    Easier to read :D Hello if you read this stranger

--- JavaScript/ImageToggle.js
+++ JavaScript/ImageToggle.js
@@ -4,12 +4,23 @@ $(document).ready(function(){
   $("#pane2").fadeToggle("fast");
   $("#pane3").fadeToggle("fast");
 //The section below are listeners for image clicks or div clicks
-  $("#team1").click(function(){$("#pane1").fadeToggle("slow");});
-  $("#team2").click(function(){$("#pane2").fadeToggle("slow");});
-  $("#team3").click(function(){$("#pane3").fadeToggle("slow");});
-  $("#teamimage").click(function(){$("#pane1").fadeToggle("slow");});
-  $("#teamimage2").click(function(){$("#pane2").fadeToggle("slow");});
-  $("#teamimage3").click(function(){$("#pane3").fadeToggle("slow");});
+  $("#team1").click(function(){
+    $("#pane1").fadeToggle("slow");
+  });
+  $("#team2").click(function(){
+    $("#pane2").fadeToggle("slow");
+  });
+  $("#team3").click(function(){
+    $("#pane3").fadeToggle("slow");
+  });
+  $("#teamimage").click(function(){
+    $("#pane1").fadeToggle("slow");
+  });
+  $("#teamimage2").click(function(){
+    $("#pane2").fadeToggle("slow");
+  });
+  $("#teamimage3").click(function(){$("#pane3").fadeToggle("slow");
+  });
 
 }
 
